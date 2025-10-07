b97fdafab08db68e8e84c71d089819efb40671b5
Maciej Szalewicz <m.szalewicz@gmail.com>
2025-10-07 13:27:31 +0200

    complete chunking

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"log"
 	"os"
@@ -10,10 +11,11 @@ func main() {
 	file, err := os.Open("messages.txt")
 
 	if err != nil {
-		log.Fatal("error", "error", err)
+		log.Fatal("error: ", err)
 	}
 
 	data := make([]byte, 8)
+	line := ""
 	for {
 		n, err := file.Read(data)
 
@@ -21,6 +23,19 @@ func main() {
 			break
 		}
 
-		fmt.Printf("read: %s\n", string(data[:n]))
+		chunk := data[:n]
+
+		if index := bytes.Index(chunk, []byte("\n")); index != -1 {
+			line += string(chunk[:index])
+			chunk = chunk[index+1:]
+			fmt.Printf("read: %s\n", line)
+			line = ""
+		}
+
+		line += string(chunk)
+	}
+
+	if len(line) != 0 {
+		fmt.Print("read: ", line, "\n")
 	}
 }
